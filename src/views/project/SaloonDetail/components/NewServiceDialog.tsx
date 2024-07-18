0134f992216c28abbd9df67080e36f78776a2dc0
root <bassilalqadi.ba@gmail.com>
2024-07-18 14:56:32 +0300

    some changes

--- src/views/project/SaloonDetail/components/NewServiceDialog.tsx
+++ src/views/project/SaloonDetail/components/NewServiceDialog.tsx
@@ -24,6 +24,7 @@ const NewServiceDialog = ({ saloonCategories, saloonStaff, fetchData }: any) => 
             isOpen={newServiceDialog}
             onClose={onDialogClose}
             onRequestClose={onDialogClose}
+            className="max-h-96 overflow-y-auto mt-4 px-4"
         >
             <h4>إضافة خدمة جديدة</h4>
             <div className="mt-4">
