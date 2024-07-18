50d3b3177fbc3f81972af0c84248b1b76218b622
root <bassilalqadi.ba@gmail.com>
2024-07-18 15:07:39 +0300

    some changes

--- src/views/project/BannersList/components/ProjectListContent.tsx
+++ src/views/project/BannersList/components/ProjectListContent.tsx
@@ -115,7 +115,9 @@ const ProjectListContent = () => {
             onClose={onEditDialogClose}
             onRequestClose={onEditDialogClose}
         >
+            <div className="max-h-96 overflow-y-auto mt-4 px-4">
             <EditBannerForm banner={selectedBanner} />
+            </div>
         </Dialog>
         </>
     )
